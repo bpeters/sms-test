d22aa3a044f6d7962cd1521a42692b6a2d3a90f8
Brennen Peters <brennen.peters@civitaslearning.com>
2015-03-10 00:22:25 -0500

    Test reply message

--- routes/index.js
+++ routes/index.js
@@ -58,5 +58,24 @@ exports.index = function(req, res) {
 
 exports.message = function(req, res) {
 	console.log(req.body);
+	client.sendMessage({
+
+			to: req.body.From, // Any number Twilio can deliver to
+			from: '+15125809414', // A number you bought from Twilio and can use for outbound communication
+			body: 'Why hello ' + req.body.body // body of the SMS message
+
+	}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+			if (!err) { // "err" is an error received during the request, if any
+
+					// "responseData" is a JavaScript object containing data received from Twilio.
+					// A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+					// http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+					console.log(responseData.from); // outputs "+14506667788"
+					console.log(responseData.body); // outputs "word to your mother."
+
+			}
+	});
 	res.send(null);
 };
